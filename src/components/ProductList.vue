f002f8fdd62c41b1e8fefef6146b669f1c151bb6
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-02-01 11:40:53 +0200

    Initial commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1>Product List</h1>
+        <img 
+            v-if="loading"
+            src="https://imgur.com/JfPpwOA.gif"/>
+        <ul v-else>
+            <li v-for="product in products"> 
+                
+                {{ product.title }} - {{ product.price }} 
+
+                <button @click="addProductToCart(product)">Add To Cart</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            loading: false
+        }
+    },
+    computed :{
+        products () {
+            return this.$store.getters.availableProducts
+        }
+    },
+    methods:{
+        addProductToCart(product){
+            this.$store.dispatch('addProductToCart', product)
+        }
+
+    },
+
+    created(){
+        this.loading = true
+        this.$store.dispatch('fetchProducts')
+        .then(()=> this.loading = false )
+    }
+}
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,86 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import { data } from 'autoprefixer'
+import shop from '@/api/shop.js'
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state:{ //= data
+        products:[],
+        //{id, quantity}
+        cart : []
+    },
+
+    getters:{//= computed properties 
+      
+        availableProducts(state, getters){
+            return state.products.filter(product => product.inventory > 0) 
+        },
+        cartProducts(state){
+            return state.cart.map(cartItem =>{
+                const product = state.products.find(product => product.id === cartItem.id)
+                return {
+                    title: product.title,
+                    price: product.price,
+                    quantity: cartItem.quantity
+                }
+            })
+        }
+    },
+    
+    actions: { //store's methods
+        fetchProducts( { commit }){
+            //make the call
+            // run setProducts mutation
+            
+            return new Promise((resolve, reject) => {
+
+                shop.getProducts(products => {
+                    commit('setProducts', products)
+                
+                    resolve()
+                })
+            })
+            
+        },
+
+        addProductToCart(context, product){
+            if (product.inventory > 0){
+            const cartItem = context.state.cart.find(item => item.id === product.id)
+            if(!cartItem){
+                //pushProductToCart
+                context.commit('pushProductToCart', product.id)
+            } else{
+                //incrementItemQuantity
+                context.commit('incrementItemQuatity', cartItem)
+            }
+            context.commit('decrementProductInventory', product)            
+
+            }
+          
+        }
+     
+    },
+
+
+    mutations:{
+        setProducts (state, products) {
+            // update products
+            state.products = products
+        },
+
+        pushProductToCart(state, productId){
+            state.cart.push({
+                id: productId,
+                quantity: 1
+            })
+        },
+        incrementItemQuatity(state, cartItem){
+            cartItem.quantity++
+        },
+        decrementProductInventory(state, product){
+            product.inventory--
+        }
+    }
+
+})
