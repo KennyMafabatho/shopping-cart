6eae79ceaa37d095839e5505b4c720d95a7f9c9d
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-03-09 10:36:49 +0200

    Installed popper and jquery

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -20,16 +20,39 @@
        <footer class="">
         <h4 class="pt-4 pb-4 font-weight-bold text-uppercase">Inventory Management</h4>
         <ul class="align-items-centre">
+       
         <li v-for="product in products "> 
             
-        {{ product.title }} - {{ product.price | currency }} <span class="pl-2 pb-2"> <button type="button" class ="btn btn-danger" @click="removeProduct(product)"> Delete </button> </span> 
+        {{ product.title }} - {{ product.price | currency }} <span class="pl-2 pb-2"> <button type="button" class ="btn btn-danger" data-toggle="modal" data-target="#ModalID"> Delete </button> </span> 
         <pre class="mt-2"> <button type="button" class ="btn btn-info" @click="decrementInventory(product)" > - </button> {{ product.inventory}} <button type="button" class ="btn btn-info" @click="incrementInventory(product)"> + </button> </pre>
                        
          </li>  
         </ul>
+
+         <div class="modal fade" id="modalID" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="ModalLabel">Delete Product</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"> </span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Are you sure you want to delete this product?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-danger" @click="removeProduct(product)" >Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         
 
         </footer>
+
+      
     </div>
 
     
@@ -72,7 +95,7 @@ export default {
             incrementInventory:'products/incrementInventory',
             decrementInventory:'products/decrementInventory',
             addProduct:'products/addProductToList',
-           // removeProduct:'products/removeProduct'
+           removeProduct:'products/removeProductFromList'
         }),
       
     },
@@ -83,3 +106,18 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.modal {
+  display: none; 
+  position: fixed; 
+  z-index: 1; 
+  left: 0;
+  top: 0;
+  width: 100%; 
+  height: 100%; 
+  overflow: auto; 
+  background-color: #474e5d;
+  padding-top: 50px;
+}
+</style>
