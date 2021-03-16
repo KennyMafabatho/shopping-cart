8478c95d53535eeeeba2b99585b171804b5727f1
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-03-16 08:49:02 +0200

    Modified the admin page and modal

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -24,55 +24,30 @@
         <li v-for="product in products "> 
        
         {{ product.title }} - {{ product.price | currency }} <span class="pl-2 pb-2"> 
-           <button type="button" class="btn btn-danger"  @click="showModal = true">Delete</button> </span>
+           <button type="button" class="btn btn-danger"  @click="$refs.modalname.openModal()">Delete</button> </span>
         <pre class="mt-2"> <button type="button" class ="btn btn-info" @click="decrementInventory(product)" > - </button> {{ product.inventory}} <button type="button" class ="btn btn-info" @click="incrementInventory(product)"> + </button> </pre>
                        
          </li>  
         </ul>
-         <modal v-if="showModal">
+         <modal ref="modalname" >
                   <template v-slot:header>
-                    <h3>Deleting a product</h3>
+                    <h3 >Deleting a product</h3>
                   </template>
 
-                  <template v-slot:body> <h4> Are you sure you want to delete {{ product.name }} ? </h4>
+                  <template v-slot:body> <h4 v-if="product"> Are you sure you want to delete {{ product.name }} ? </h4>
                 </template>
 
                   <template v-slot:footer>
-                    <div class="d-flex align-items-center justify-content-between">
-                      <button class="btn btn--secondary" @click="closeModal()">Cancel</button>
-                      <button class="btn btn-danger" @click="removeProduct(product)">Delete</button>
+                    <div class="align-items-center justify-content-between  mx-auto ">
+                      <button class="btn btn-secondary" @click="$refs.modalname.closeModal()">Cancel</button>
+                      <button class="btn btn-danger" @click="removeProduct(this.$store.product)">Delete</button>
                     </div>
                   </template>
           </modal>
 
-         <!-- <div class="modal fade" id="modalID" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="ModalLabel">Delete Product</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true"> </span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Are you sure you want to delete this product?</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-danger" @click="removeProduct(product)" >Delete</button>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-        
-
         </footer>
-
-      
     </div>
 
-    
-    
 </template>
 <script>
 import {mapState, mapGetters, mapActions} from 'vuex'
@@ -89,7 +64,7 @@ export default {
             price:'',
             inventory:''
         },
-        showModal: false
+       
     }},
     
       
@@ -113,7 +88,7 @@ export default {
             incrementInventory:'products/incrementInventory',
             decrementInventory:'products/decrementInventory',
             addProduct:'products/addProductToList',
-           removeProduct:'products/removeProductFromList'
+            removeProduct:'products/removeProductFromList'
         }),
          
       
@@ -127,16 +102,29 @@ export default {
 </script>
 
 <style scoped>
-.modal {
-  display: none; 
-  position: fixed; 
-  z-index: 1; 
-  left: 0;
-  top: 0;
-  width: 100%; 
-  height: 100%; 
-  overflow: auto; 
-  background-color: #474e5d;
-  padding-top: 50px;
+.overflow-hidden {
+  overflow: hidden;
 }
+.btn {
+  padding: 8px 16px;
+  border-radius: 5px;
+  }
+
+overflow-hidden {
+  overflow: hidden;
+}
+.mx-auto {
+  margin-left: auto;
+  margin-right: auto;
+}
+.d-flex {
+  display: flex;
+}
+.align-items-center {
+  align-items: center;
+}
+.justify-content-between {
+  justify-content: space-between;
+}
+
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,18 +1,16 @@
 <template>
   <transition name="fade">
-    <div class="modal">
+    <div class="modal-mask" v-if="showModal">
       <div class="modal-wrapper">
         <div class="modal-container">
           <div class="modal-header">
             <slot name="header">    
             </slot>
           </div>
-​
           <div class="modal-body">
             <slot name="body">   
             </slot>
           </div>
-​
           <div class="modal-footer">
             <slot name="footer">
             </slot>
@@ -23,78 +21,113 @@
   </transition>
 </template>
 <script>
+import {mapState, mapGetters, mapActions} from 'vuex'
+import products from '../store/modules/products'
 
 export default {
   name: "Modal",
   data() {
+    
     return {
       showModal: false
     };
+    
+  },
+  
+  computed:{
+    ...mapState({
+            products: state => state.products.items}),
+
+            ...mapGetters('products', {
+            productIsInStock: 'productIsInStock'
+        }),
   },
   methods: {
     closeModal() {
       this.showModal = false;
+       document.querySelector("body").classList.remove("overflow-hidden");
     },
     openModal() {
       this.showModal = true;
-    }
+      document.querySelector("body").classList.add("overflow-hidden");
+    },
+    deleteProduduct(){
+      this.showModal = false
+      let index = this.items.indexOf(this.selectedProduct);
+      this.products.splice(index, 1);
+      this.selectedProduct = null;
+    },
+    ...mapActions({
+    removeProduct:'products/removeProductFromList'})
   }
-};
+}
+
 </script>
 
 <style scoped>
-
 .modal {
-  overflow-x: hidden;
-  overflow-y: auto;
-  position: fixed;
+ position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
   z-index: 9;
-}  backdrop {
-    background-color: rgba(0, 0, 0, 0.3);
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 1;
-  }
-  dialog {
-    background-color: #ffffff;
+  overflow-x: hidden;
+  overflow-y: auto;
+} .modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+    
     position: relative;
     width: 600px;
+    background-color: #ffffff;
+    border-radius: 5px;
     margin: 50px auto;
     display: flex;
     flex-direction: column;
-    border-radius: 5px;
     z-index: 2;
     @media screen and (max-width: 992px) {
       width: 90%;
-    }
-  }
-  close {
-    width: 30px;
-    height: 30px;
-  }
-  header {
-    padding: 20px 20px 10px;
+}
+}
+.modal-header h3 {
     display: flex;
     align-items: flex-start;
     justify-content: space-between;
-  }
-  body {
+    padding: 20px 20px 10px;
+}
+
+.modal-body {
+  display: none;
     padding: 10px 20px 10px;
     overflow: auto;
     display: flex;
     flex-direction: column;
     align-items: stretch;
-  }
-  footer {
-    padding: 10px 20px 20px;
-  }
+}
+.modal-footer {
+  padding: 10px 20px 20px;
+}
+
+.modal-default-button {
+  float: right;
+}
+
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.2s;
