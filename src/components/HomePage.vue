cda18bbccb75dcf88d678d3104f1362a842fb5a1
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-06-15 10:28:12 +0200

    Created home page and added new product images

---
+++ src/components/HomePage.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="home pt-4">
+      <div class="container mt-4" >
+        <div class="d-flex mt-4 mb-4">
+          <div class="col-lg-3 col-md-3 col-sm-6 item">
+            <router-link to="/products" class="pr-2">
+              <div class="mb-2">
+                Designer T-shirts 
+               </div>
+               <span><img src="@/assets/H&M t-shirt.jpg"/></span>
+            </router-link> 
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-6 item">
+            <router-link to="/products" class="pr-2">
+              <div class="mb-2">Watches
+              </div> 
+              <span> <img src="@/assets/watches.jpg"/> </span>
+            </router-link> 
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-6 item">
+            <router-link to="/products" class="pr-2"> <div class="mb-2">Shoes</div>
+                <span> <img src="@/assets/shoes.jpg"/> </span>
+            </router-link> 
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-6 item">
+            <router-link to="/products" class="pr-2"> <div class="mb-2">Ipads </div>
+                <span> <img src="@/assets/Ipad4.jpg"/> </span>
+            </router-link> 
+          </div>
+         
+        </div>
+      </div>
+      <div class="text-center">
+      <button class="btn btn-outline-secondary " @click="products">And more</button>
+      </div>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+ 
+  computed:{
+    products(){
+      return +'/products';
+    }
+  }
+  }
+</script>
+
+<style scoped>
+.container{
+  padding-top: 4rem;
+}
+ body {
+    margin: 0;
+    padding-top: 2rem;
+  }
+ header{
+ color:#8b8885 ;
+}
+
+h2,h4,h5{
+ color:#26242b;
+ margin: 1em; 
+}
+
+ button{
+ border:1px  solid #00f97c;
+ background-color: black;
+ color:#fff;
+}
+.home{
+   
+  }
+  img{
+    max-width: 200px;
+    height: 150px;
+  }
+  a{
+    color:lightseagreen;
+    text-decoration: none;
+    
+  }
+  a:hover,
+  a:visited{
+    text-decoration: underline;
+  }
+
+ .item{
+  color: #fff;
+  text-align: center;
+  border-bottom-left-radius: 93%;
+  position: relative;
+  transition:.3s ease-in-out;
+ }
+</style>
