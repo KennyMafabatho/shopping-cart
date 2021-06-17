e7d0fe41f170b118aac836932d9d74239e08ff96
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-06-17 17:49:38 +0200

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,40 +1,38 @@
 <template>
-  <div class="home pt-4">
+
       <div class="container mt-4" >
         <div class="row mt-4 mb-4">
-          <div class="col-lg-3 col-md-3 col-6 item">
+          <div class="col-md-3 col item">
+                <p class="mb-2"> Designer T-shirts </p>
             <router-link to="/products" class="pr-2">
-              <p class="mb-2">
-                Designer T-shirts 
-               </p>
                <span><img src="@/assets/H&M t-shirt.jpg"/></span>
             </router-link> 
           </div>
-          <div class="col-lg-3 col-md-3 col-6 item">
+          <div class="col-md-3 col-6 item">
+            <p class="mb-2">Watches</p> 
             <router-link to="/products" class="pr-2">
-              <p class="mb-2">Watches
-              </p> 
               <span> <img src="@/assets/watches.jpg"/> </span>
             </router-link> 
           </div>
-          <div class="col-lg-3 col-md-3 col-6 item">
+          <div class="col-md-3 col-6 item">
+            <p class="mb-2">Shoes</p>
             <router-link to="/products" class="pr-2"> 
-              <p class="mb-2">Shoes</p>
+              
                 <span> <img src="@/assets/shoes.jpg"/> </span>
             </router-link> 
           </div>
-          <div class="col-lg-3 col-md-3 col-6 item">
+          <div class="col-md-3 col-6 item">
+            <p class="mb-2"> Ipads </p>
             <router-link to="/products" class="pr-2"> 
-                <p class="mb-2"> Ipads </p>
+                
                 <span> <img src="@/assets/Ipad4.jpg"/> </span>
             </router-link> 
           </div>
         </div>
         <div class="text-center pt-4">
-          <a href="/products" class="btn btn-secondary">And more </a>
+          <a href="/products" class="btn btn-dark active">And more </a>
         </div>
       </div>
-  </div>
 </template>
 
 
@@ -66,26 +64,22 @@ export default {
 }
 
 h2,h4,h5{
- color:#26242b;
- margin: 1em; 
+ color:#26242b; 
 }
 
- button{
+ .button{
  border:1px  solid #00f97c;
  background-color: black;
  color:#fff;
 }
-.home{
-   
-  }
+
   img{
     max-width: 200px;
     height: 150px;
   }
   a{
-    color: black;
+    color:white;
     text-decoration: none;
-    
   }
   a:hover,
   a:visited{
