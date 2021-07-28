eb8ac0c3f4fabd571c96983ef67429d73980ab05
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-07-28 15:08:23 +0200

    Registered fontawesome icons

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import App from './App'
-import store from '@/store/index'
-import router from '@/router/index'
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import App from './App';
+import store from '@/store/index';
+import router from '@/router/index';
+
+
 
 Vue.use(BootstrapVue)
 
@@ -12,9 +18,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import {currency} from "@/currency";
 
+
+library.add(fab);
+library.add(fas);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false
 Vue.filter('currency', currency)
 /* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   render: h => h(App),
